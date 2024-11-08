2390568c6cd1a394b2b64641640d86917fac71f6
Sachini <sachichathudissanayaka@gmail.com>
2024-11-08 19:48:28 +0530

    Add changes

--- src/components/home/SingleBookCard.jsx
+++ src/components/home/SingleBookCard.jsx
@@ -1,62 +1,73 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { MdOutlineDelete } from "react-icons/md";
-import { IoInformationCircleOutline } from "react-icons/io5";
+// import { AiOutlineEdit } from "react-icons/ai";
+// import { MdOutlineDelete } from "react-icons/md";
+// import { IoInformationCircleOutline } from "react-icons/io5";
 import { Badge, Box, Card, HStack, Image, Flex } from "@chakra-ui/react";
 import { GridItem } from "@chakra-ui/react";
 
 const SingleBookCard = ({ book }) => {
   return (
-    <GridItem key={book._id}>
-      <Flex flexDir={"column"} alignItems={"center"}>
-        <Card.Root maxW="xl" overflow="hidden">
-          <Card.Body p={5} >
-            <Flex flexDir={{ base: "column", md: "row" }} alignItems={"center"}>
-              <Image
-                h={"170px"}
-                src={book.coverImage}
-                alt={book.title}
-                rounded={"md"}
-              />
-              <Box ml={{ base: 0, md: 5 }}>
-                <Card.Title>{book.title}</Card.Title>
-                <HStack mt="3">
-                  {book.status === "completed" ? (
-                    <Badge colorPalette={"green"} variant="subtle">
-                      {book.status}
+    <Link to={`/books/details/${book._id}`}>
+      <GridItem key={book._id} _hover={{ transform: "translateY(-5px)" }}>
+        <Flex flexDir={"column"} alignItems={"center"}>
+          <Card.Root maxW="xl" overflow="hidden">
+            <Card.Body p={3}>
+              <Flex
+                flexDir={{ base: "column", md: "row" }}
+                alignItems={"center"}
+              >
+                <Image
+                  h={"170px"}
+                  src={book.coverImage}
+                  alt={book.title}
+                  rounded={"md"}
+                />
+                <Box ml={{ base: 0, md: 5 }}>
+                  <Card.Title display={"flex"} alignItems={"center"}>
+                    {book.title}
+                  </Card.Title>
+                  <HStack mt="3">
+                    {book.status === "completed" ? (
+                      <Badge colorPalette={"green"} variant="subtle">
+                        {book.status}
+                      </Badge>
+                    ) : (
+                      <Badge colorPalette={"yellow"} variant="subtle">
+                        {book.status}
+                      </Badge>
+                    )}
+                    <Badge colorPalette={"purple"} variant="subtle">
+                      {book.pageCount} pages
                     </Badge>
-                  ) : (
-                    <Badge colorPalette={"yellow"} variant="subtle">
-                      {book.status}
-                    </Badge>
-                  )}
-                  <Badge colorPalette={"purple"} variant="subtle">
-                    {book.pageCount} pages
-                  </Badge>
-                </HStack>
-                <Card.Description mt="3">
-                  Author: {book.author}
-                </Card.Description>
-                <Card.Description mt="3">ISBN: {book.isbn}</Card.Description>
+                  </HStack>
+                  <Card.Description mt="3">
+                    Author: {book.author}
+                  </Card.Description>
+                  <Card.Description mt="3">ISBN: {book.isbn}</Card.Description>
 
-                <HStack mt={5} display={"flex"} justifyContent={"space-around"}>
-                  <Link to={`/books/details/${book._id}`}>
-                    <IoInformationCircleOutline color="#4A8A31" />
-                  </Link>
-                  <Link to={`/books/edit/${book._id}`}>
-                    <AiOutlineEdit color="#CEB038" />
-                  </Link>
-                  <Link to={`/books/delete/${book._id}`}>
-                    <MdOutlineDelete color="#A71819" />
-                  </Link>
-                </HStack>
-              </Box>
-            </Flex>
-          </Card.Body>
-        </Card.Root>
-      </Flex>
-    </GridItem>
+                  {/* <HStack
+                    mt={5}
+                    display={"flex"}
+                    justifyContent={"space-around"}
+                  >
+                    <Link to={`/books/details/${book._id}`}>
+                      <IoInformationCircleOutline color="#4A8A31" />
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit color="#CEB038" />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete color="#A71819" />
+                    </Link>
+                  </HStack> */}
+                </Box>
+              </Flex>
+            </Card.Body>
+          </Card.Root>
+        </Flex>
+      </GridItem>
+    </Link>
   );
 };
 

7baadd29b91c06fadfaa02fb9bc9de8335deff84
Sachini <sachichathudissanayaka@gmail.com>
2024-11-11 18:13:25 +0530

    Add changes

--- src/components/home/BookTable.jsx
+++ src/components/home/BookTable.jsx
@@ -2,18 +2,18 @@
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { MdOutlineDelete } from "react-icons/md";
-// import { IoInformationCircleOutline } from "react-icons/io5";
-import { HStack, Table} from "@chakra-ui/react";
-
+import { HStack, Table } from "@chakra-ui/react";
 
 const BookTable = ({ books }) => {
   return (
     <Table.Root maxW={"800px"} variant={"outline"} showColumnBorder="true">
       <Table.Header h={{ md: "32px", lg: "50px" }}>
         <Table.Row>
-          <Table.ColumnHeader  textAlign={"center"} w={"30px"}>No</Table.ColumnHeader>
+          <Table.ColumnHeader textAlign={"center"} w={"30px"}>
+            No
+          </Table.ColumnHeader>
           <Table.ColumnHeader textAlign={"center"}>Title</Table.ColumnHeader>
-          <Table.ColumnHeader textAlign={"center"} >
+          <Table.ColumnHeader textAlign={"center"}>
             Operations
           </Table.ColumnHeader>
         </Table.Row>
@@ -21,13 +21,12 @@ const BookTable = ({ books }) => {
       <Table.Body>
         {books.map((book, index) => (
           <Table.Row key={book._id}>
-            <Table.Cell textAlign={"center"} w={"30px"}>{index + 1}</Table.Cell>
+            <Table.Cell textAlign={"center"} w={"30px"}>
+              {index + 1}
+            </Table.Cell>
             <Table.Cell textAlign={"center"}>{book.title}</Table.Cell>
             <Table.Cell display={"flex"} justifyContent={"space-around"}>
               <HStack>
-                {/* <Link to={`/books/details/${book._id}`}>
-                  <IoInformationCircleOutline color="#4A8A31" />
-                </Link> */}
                 <Link to={`/books/edit/${book._id}`}>
                   <AiOutlineEdit color="#CEB038" />
                 </Link>

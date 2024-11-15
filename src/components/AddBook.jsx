376092ac40c94106693757d3c84cffb2ff670832
Sachini <sachichathudissanayaka@gmail.com>
2024-11-15 22:23:00 +0530

    Add changes

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -5,15 +5,15 @@ import { Flex, Text } from "@chakra-ui/react";
 
 const AddBook = () => {
   return (
-    <Flex h={16} alignItems={"center"}>
-        <Text>Add a book</Text>
-        <Link to="/books/create">
-          <Button size="sm" variant="outline" marginLeft="3">
-            <BsPlusSquare />
-          </Button>
-        </Link>
-      </Flex>
-  )
-}
+    <Flex h="16" alignItems="center">
+      <Text>Add a book</Text>
+      <Link to="/books/create">
+        <Button size="sm" variant="outline" marginLeft="3">
+          <BsPlusSquare />
+        </Button>
+      </Link>
+    </Flex>
+  );
+};
 
-export default AddBook
+export default AddBook;

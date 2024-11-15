60b2817842c3003fa4826a73dca1f44985f38d48
Sachini <sachichathudissanayaka@gmail.com>
2024-11-15 22:22:17 +0530

    Add changes

--- src/components/home/SingleBookCard.jsx
+++ src/components/home/SingleBookCard.jsx
@@ -6,34 +6,31 @@ const SingleBookCard = ({ book }) => {
   return (
     <Link to={`/books/details/${book._id}`}>
       <GridItem key={book._id} _hover={{ transform: "translateY(-5px)" }}>
-        <Flex flexDir={"column"} alignItems={"center"}>
+        <Flex flexDir="column" alignItems="center">
           <Card.Root maxW="xl" overflow="hidden">
-            <Card.Body p={3}>
-              <Flex
-                flexDir={{ base: "column", md: "row" }}
-                alignItems={"center"}
-              >
+            <Card.Body p="3">
+              <Flex flexDir={{ base: "column", md: "row" }} alignItems="center">
                 <Image
-                  h={"170px"}
+                  h="170px"
                   src={book.coverImage}
                   alt={book.isbn}
-                  rounded={"md"}
+                  rounded="md"
                 />
                 <Box ml={{ base: 0, md: 5 }}>
-                  <Card.Title display={"flex"} alignItems={"center"}>
+                  <Card.Title display="flex" alignItems="center">
                     {book.title}
                   </Card.Title>
                   <HStack mt="3">
                     {book.status === "Completed" ? (
-                      <Badge colorPalette={"green"} variant="subtle">
+                      <Badge colorPalette="green" variant="subtle">
                         {book.status}
                       </Badge>
                     ) : (
-                      <Badge colorPalette={"yellow"} variant="subtle">
+                      <Badge colorPalette="yellow" variant="subtle">
                         {book.status}
                       </Badge>
                     )}
-                    <Badge colorPalette={"purple"} variant="subtle">
+                    <Badge colorPalette="purple" variant="subtle">
                       {book.pageCount} pages
                     </Badge>
                   </HStack>

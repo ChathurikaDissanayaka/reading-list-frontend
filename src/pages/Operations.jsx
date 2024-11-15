d8b99588f7cadaea518cab3dcfbd17fa53a7a6d6
Sachini <sachichathudissanayaka@gmail.com>
2024-11-16 00:52:24 +0530

    Add changes

--- src/pages/Operations.jsx
+++ src/pages/Operations.jsx
@@ -30,23 +30,26 @@ const Operations = () => {
   return (
     <Box p="4">
       <Navbar />
-      {loading && <LoadingSpinner />}
-      <Flex alignItems="center" flexDir="column">
-        <Box w={{ base: "95%", md: "500px", lg: "800px" }}>
-          <Text
-            fontSize={{
-              base: "16px",
-              sm: "25px",
-            }}
-            fontWeight="bold"
-            mb="3"
-          >
-            Add, edit or update books
-          </Text>
-          <AddBook />
-          {bookCount === 0 ? <NoBooksFound /> : <BookTable books={books} />}
-        </Box>
-      </Flex>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Flex alignItems="center" flexDir="column">
+          <Box w={{ base: "95%", md: "500px", lg: "800px" }}>
+            <Text
+              fontSize={{
+                base: "16px",
+                sm: "25px",
+              }}
+              fontWeight="bold"
+              mb="3"
+            >
+              Add, edit or update books
+            </Text>
+            <AddBook />
+            {bookCount === 0 ? <NoBooksFound /> : <BookTable books={books} />}
+          </Box>
+        </Flex>
+      )}
     </Box>
   );
 };
